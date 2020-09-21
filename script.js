07e28390633578ba90f8d7204dc25b799c3cbda2
Bhargava Patel <bhargavapatel82@gmail.com>
2020-09-21 11:27:53 -0400

    added the user input.

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ var userChoice = {};
 var charLength = parseInt(prompt("Please choose the length of the password between 8 and 128 characters"));
 
 console.log(charLength)
+
+// user input
+function promptUser() {
+  var numChar = confirm("Would you like numbers?");
+  var lowerChar = confirm("Would you like lowercase characters?");
+  var upperChar = confirm("Would you like uppercase characters?");
+  var specialChar = confirm("Would you like special characters?");
+}
+
+if (numChar) {
+  userChoice["numEl"] = numEl;
+}
+if (lowerChar) {
+  userChoice["lowerEl"] = lowerEl;
+}
+if (upperChar) {
+  userChoice["upperEl"] = upperEl;
+}
+if (special) {
+  userChoice["specialEl"] = specialEl;
+}
